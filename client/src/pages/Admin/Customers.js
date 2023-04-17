ef043e29ac0919f09ac652cb0fab24472a916aef
Joel B Koshy <joelbkoshy@gmail.com>
2023-04-18 02:02:52 +0530

    Payment History + Virtual Cart History

--- client/src/pages/Admin/Customers.js
+++ client/src/pages/Admin/Customers.js
@@ -4,6 +4,8 @@ import SideBar from '../../components/Admin SideBar/SideBar';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './Stores.css';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 import PageviewOutlinedIcon from '@mui/icons-material/PageviewOutlined';
 import DeleteIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import Axios from 'axios';
@@ -13,6 +15,33 @@ function Customers() {
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 10; // Number of items to show per page
 
+  const [open, setOpen] = React.useState(false);
+
+  function handleOpen(cid) {
+    setSelectedCid(cid);
+    setOpen(true);
+  }
+
+  const handleClose = () => setOpen(false);
+  const [selectedCid, setSelectedCid] = useState(null);
+
+  const [payHis, setPayHis] = useState([])
+
+  const tableModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    width: 950,
+    height: 600,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
   useEffect(() => {
     Axios.get('http://localhost:3003/api/read_customers')
       .then(res => {
@@ -21,6 +50,17 @@ function Customers() {
       })
       .catch(err => console.log(err));
   }, []);
+
+  useEffect(() => {
+    if (selectedCid) {
+      Axios.get(`http://localhost:3003/api/get_user_payhis/${selectedCid}`)
+        .then((res) => {
+          console.log(res);
+          setPayHis(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [selectedCid]);
 
   function handlePageChange({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
@@ -73,7 +113,57 @@ function Customers() {
                     <td>{customer.gender}</td>
                     <td>{formattedDate}</td>
                     <td className='d-flex justify-content-between'>
-                      <button className='btn'><PageviewOutlinedIcon /></button>
+                      <button className='btn' onClick={() => handleOpen(customer.cid)}c><PageviewOutlinedIcon /></button>
+                      <Modal
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="modal-modal-title"
+                          aria-describedby="modal-modal-description"
+                        >
+                          <Box sx={tableModalStyle}>
+                            {
+                              <div style={{ margin: 10, marginLeft: 10, marginRight: 10, justifyContent: 'center' }}>
+                                <div style={{display:'flex',justifyContent:'center'}}>
+                                <h3>Payment History</h3>
+                                </div>
+                                <table className="table table-hover mt-3 table-bordered " >
+                                  <thead>
+                                    <tr className='table-dark'>
+                                      <th scope="col">Payment ID</th>
+                                      <th scope="col">Virtual Cart ID</th>
+                                      <th scope="col">Payment Date</th>
+                                      <th scope="col">Payment Time</th>
+                                      <th scope="col">Total Payment</th>
+                                      {/* <th scope="col">Date of Birth</th>
+                <th scope="col">Actions</th> */}
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+               {payHis.map((payment) => {
+                  const dob = new Date(payment.pay_time);
+                  const formattedDate = dob.toLocaleDateString();
+                  const time = new Date(payment.pay_time)
+                  const formattedTime = time.toLocaleTimeString();
+
+                  return (
+                    <tr key={payment.pay_id}>
+                      <td>{payment.pay_id}</td>
+                      <td>{payment.vc_id}</td>
+                      <td>{formattedDate}</td>
+                      <td>{formattedTime}</td>
+                      <td>{payment.total_pay}</td>
+                      </tr>
+                         )
+               } ) }
+                                  </tbody>
+                                </table>
+
+                              </div>
+
+
+                            }
+                          </Box>
+                        </Modal>
                       <button className='btn' onClick={() => handleDelete(customer.cid)}><DeleteIcon /></button>
                     </td>
                   </tr>
--- client/src/pages/Admin/Payments.js
+++ client/src/pages/Admin/Payments.js
@@ -14,10 +14,15 @@ import Axios from 'axios'
 function Customers() {
 
   const [payments, setPayments] = useState([])
-  // const [open, setOpen] = React.useState(false);
-  // const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false);
 
+  function handleOpen(pay_id) {
+    setSelectedPayId(pay_id);
+    setOpen(true);
+  }
+
+  const handleClose = () => setOpen(false);
+  const [selectedPayId, setSelectedPayId] = useState(null);
   // const style = {
   //   position: 'absolute',
   //   top: '50%',
@@ -33,20 +38,20 @@ function Customers() {
   //   alignItems: 'center'
   // };
 
-  // const tableModalStyle = {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: 'translate(-50%, -50%)',
-  //   bgcolor: 'background.paper',
-  //   border: '2px solid #000',
-  //   boxShadow: 24,
-  //   width: 400,
-  //   height: 400,
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  //   alignItems: 'center'
-  // };
+  const tableModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    width: 950,
+    height: 600,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
 
   useEffect(() => {
     Axios.get('http://localhost:3003/api/read_payments')
@@ -58,41 +63,47 @@ function Customers() {
     console.log("Testing: " + payments)
   }, []);
 
-  // function handlePayments(pay_id) {
-    // console.log("Image address : " + pimg);
-    // setImg(pimg.toString('utf8'));
-    // console.log("After Image address : " + img);
-  //   handleOpen();
-  // }
+  useEffect(() => {
+    if (selectedPayId) {
+      Axios.get(`http://localhost:3003/api/get_pay_products/${selectedPayId}`)
+        .then((res) => {
+          console.log(res);
+          setPayProd(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [selectedPayId]);
 
-  // const [payTable, setPayTable] = useState([])
+
+
+  const [payProd, setPayProd] = useState([])
 
   return (
     <div className='Admin'>
       <SideBar />
-    
 
-    <div class="AdminStores-container">
-      <>
-        <div className='Customers-right mt-4' style={{display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
-         <h3 >Payments</h3>
-          <table className="table table-hover mt-3 table-bordered ">
-            <thead>
-              <tr className='table-dark'>
-                <th scope="col">Payment ID</th>
-                <th scope="col">Customer ID</th>
-                <th scope="col">Customer Name</th>
-                <th scope="col">Virtual Cart ID</th>
-                <th scope="col">Total Payment</th>
-                <th scope="col">Payment Date</th>
-                <th scope="col">Payment Time</th>
-                {/* <th scope="col">Actions</th> */}
-                {/* <th scope="col">Date of Birth</th>
+
+      <div class="AdminStores-container">
+        <>
+          <div className='Customers-right mt-4' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+            <h3 >Payments</h3>
+            <table className="table table-hover mt-3 table-bordered ">
+              <thead>
+                <tr className='table-dark'>
+                  <th scope="col">Payment ID</th>
+                  <th scope="col">Customer ID</th>
+                  <th scope="col">Customer Name</th>
+                  <th scope="col">Virtual Cart ID</th>
+                  <th scope="col">Total Payment</th>
+                  <th scope="col">Payment Date</th>
+                  <th scope="col">Payment Time</th>
+                  <th scope="col">Actions</th>
+                  {/* <th scope="col">Date of Birth</th>
                 <th scope="col">Actions</th> */}
-              </tr>
-            </thead>
-            <tbody>
-              {/* <tr>
+                </tr>
+              </thead>
+              <tbody>
+                {/* <tr>
               <th scope="row">100</th>
               <td>Raymond Shirt</td>
               <td>121</td>
@@ -102,51 +113,79 @@ function Customers() {
 
 
             </tr> */}
-              {payments.map((payment) => {
-                const dob = new Date(payment.pay_date);
-                const formattedDate = dob.toLocaleDateString();
-                const time = new Date(payment.pay_time)
-                const formattedTime  = time.toLocaleTimeString();
+                {payments.map((payment) => {
+                  const dob = new Date(payment.pay_time);
+                  const formattedDate = dob.toLocaleDateString();
+                  const time = new Date(payment.pay_time)
+                  const formattedTime = time.toLocaleTimeString();
 
-                return (
-                  <tr key={payment.pay_id}>
-                    <td>{payment.pay_id}</td>
-                    <td>{payment.cid}</td>
-                    <td>{payment.cname}</td>
-                    <td>{payment.vc_id}</td>
-                    <td>{payment.total_pay}</td>
-                    <td>{formattedDate}</td>
-                    <td>{formattedTime}</td>
-                    {/* <td className='d-flex justify-content-between'> */}
-                      {/* <button className='btn btn-success'>read</button> */}
-                      {/* <button className='btn'><PageviewOutlinedIcon/></button> */}
-                      {/* <Modal
+                  return (
+                    <tr key={payment.pay_id}>
+                      <td>{payment.pay_id}</td>
+                      <td>{payment.cid}</td>
+                      <td>{payment.cname}</td>
+                      <td>{payment.vc_id}</td>
+                      <td>{payment.total_pay}</td>
+                      <td>{formattedDate}</td>
+                      <td>{formattedTime}</td>
+                      <td className='d-flex justify-content-between'>
+                        {/* <button className='btn btn-success'>read</button> */}
+                        <button className='btn' onClick={() => handleOpen(payment.pay_id)}><PageviewOutlinedIcon /></button>
+                        <Modal
                           open={open}
                           onClose={handleClose}
                           aria-labelledby="modal-modal-title"
                           aria-describedby="modal-modal-description"
                         >
-                          <Box sx={tableModalStyle}> */}
-                            {/* <button className='btn' style={{ backgroundColor: 'rgb(21, 101, 192)', color: 'white', borderStyle: 'none' }}>
-                              <a onClick={downloadQR}> Download QR </a>
-                            </button> */}
+                          <Box sx={tableModalStyle}>
                             {
+                              <div style={{ margin: 10, marginLeft: 10, marginRight: 10, justifyContent: 'center' }}>
+                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                  <h3>Virtual Cart</h3>
+                                </div>
+                                <table className="table table-hover mt-3 table-bordered " >
+                                  <thead>
+                                    <tr className='table-dark'>
+                                      <th scope="col">Product ID</th>
+                                      <th scope="col">Product Name</th>
+                                      <th scope="col">Quantity</th>
+                                      <th scope="col">Price</th>
+                                      {/* <th scope="col">Date of Birth</th>
+                <th scope="col">Actions</th> */}
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    {payProd.map((prod) => {
+                                      return (
+                                        <tr key={prod.pid}>
+                                          <td>{prod.pid}</td>
+                                          <td>{prod.pname}</td>
+                                          <td>{prod.quantity}</td>
+                                          <td>{prod.price}</td>
+                                        </tr>
+                                      )
+                                    })}
+
+                                  </tbody>
+                                </table>
+
+                              </div>
+
 
                             }
-                          {/* </Box>
-                        </Modal> */}
-                      {/* <button className='btn'><DeleteIcon/></button> */}
-                    {/* </td> */}
-                  </tr>
-                );
-              })}
+                          </Box>
+                        </Modal>
+                      </td>
+                    </tr>
+                  );
+                })}
 
 
-            </tbody>
-          </table>
-        </div>
-      </>
-    </div>
+              </tbody>
+            </table>
+          </div>
+        </>
+      </div>
     </div>
   )
 }
